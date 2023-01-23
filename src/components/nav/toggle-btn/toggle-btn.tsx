3f2e106250ff624dc19f59832506d30fc637301b
Emens Fede <emens.fede@gmail.com>
2023-01-23 09:35:03 -0300

    refactor toggle button logic

--- src/components/nav/toggle-btn/toggle-btn.tsx
+++ src/components/nav/toggle-btn/toggle-btn.tsx
@@ -1,20 +1,8 @@
-import { useState } from "react";
 import "./toggle-btn.scss";
 
 function Toggle({ changeAppMode }: any) {
-  const [darkActive, setDarkActive] = useState(false);
-
-  function switchMode() {
-    setDarkActive(!darkActive);
-    changeAppMode();
-  }
-
   return (
-    <div
-      className={darkActive ? "dark-mode" : ""}
-      id="toggle"
-      onClick={switchMode}
-    >
+    <div id="toggle" onClick={changeAppMode}>
       <i className="material-icons-outlined">light_mode</i>
       <i className="material-icons-outlined">dark_mode</i>
     </div>
